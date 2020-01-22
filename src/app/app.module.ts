9a00d8705044935e49609a42f378a26be4ba04e1
devnumber22 <devnumber22@gmail.com>
2020-01-21 17:42:35 -0700

    feat: FAQ page started
     + Added FAQ component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    FaqPageComponent
   ],
   imports: [
     BrowserModule,
